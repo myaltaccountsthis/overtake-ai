d098e02c74f7b57297ecff30014ef67d5492ca66
myaltaccountsthis <71832770+myaltaccountsthis@users.noreply.github.com>
2025-10-19 07:08:58 -0500

    Docker and fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -74,7 +74,7 @@ export default function App() {
   }
   //PUT GEMINI CODE HERE
   async function handleChat(message: string) {
-    return await (await fetch("http://127.0.0.1:5000/message", {method: "POST", body: message})).text();
+    return await (await fetch(import.meta.env.VITE_BACKEND_ENDPOINT + "/message", {method: "POST", body: message})).text();
     // const m = message.toLowerCase();
     // if (m.includes("tire")) {
     //   const avg = avgTireTemp.toFixed(1);
@@ -211,7 +211,7 @@ export default function App() {
   const [suggestions, setSuggestions] = useState<{ [key: string]: string }>({});
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/get_data")
+    fetch(import.meta.env.VITE_BACKEND_ENDPOINT + "/get_data")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -998,7 +998,14 @@ export default function App() {
 
               {/* Vertical Buttons */}
               <div className="flex flex-col gap-2 w-full items-center">
-                <button className="w-24 px-3 py-1 rounded bg-green-500 text-slate-900 font-medium hover:bg-green-600 transition">
+                <button
+                  className="w-24 px-3 py-1 rounded bg-green-500 text-slate-900 font-medium hover:bg-green-600 transition"
+                  onClick={() =>
+                    speak(
+                      "Lando Norris's pace is good and the distance between the next car is closing you are 1 second away"
+                    )
+                  }
+                >
                   Play
                 </button>
                 <button className="w-24 px-3 py-1 rounded bg-yellow-400 text-slate-900 font-medium hover:bg-yellow-500 transition">
